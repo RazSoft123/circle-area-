379a70ee60e3fd6cf9b2f1a752dad85314840086
RazSoft123 <33057716+RazSoft123@users.noreply.github.com>
2017-10-24 17:21:58 +0530

    Create circle area.c

---
+++ circle area.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+/*****************************************************************************************************************************************
+Name         :Circle area 
+start date   :24-10-2017
+discription  :This program is made to solve simple mensuration problems in math 
+Cretor       :Raz
+*****************************************************************************************************************************************/
+
+char x,y,z,a,b,c,d,e,f;
+printf("\nCircle Area ");
+printf("\nEnter the name of model");
+scanf("%c%c%c%c%c%c%c%c%c",&x,&y,&z,&a,&b,&c,&d,&e,&f);
+getch();
+}
